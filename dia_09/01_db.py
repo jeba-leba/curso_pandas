455075c5ebf7a406959441d5ca1ba4139bb80829
Jebaleba <jeanbenjamim@hotmail.com>
2026-04-30 13:48:53 -0300

    feat: banco de dados

---
+++ dia_09/01_db.py
@@ -0,0 +1,20 @@
+# %%
+
+import pandas as pd
+import sqlalchemy
+
+# %%
+
+engine = sqlalchemy.create_engine("sqlite:///../data/olist.db")
+
+# %%
+
+clientes = pd.read_sql_table(table_name="tb_customers", con=engine)
+
+# %%
+
+query = "SELECT * FROM tb_customers LIMIT 100"
+
+df_100 = pd.read_sql_query(query, con=engine)
+df_100
+# %%
